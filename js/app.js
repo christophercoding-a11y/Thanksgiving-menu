7596d8a3abe84f6d7862e9f85c6bac8056c18d86
christophercodinga11y <christopherjack14@gmail.com>
2024-11-26 13:28:11 -0600

    finishedthanksgivingmenu

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ class MenuForm {
                 id: 1,
                 type: 'meat',
                 item: 'fried turkey',
-                imgUrl: 'fried_turkey.jpeg',
+                imgUrl: 'fried-turkey.jpeg',
                 isChecked: false,
                 madeBy: 'Mom'
             },
@@ -29,7 +29,7 @@ class MenuForm {
                 id: 2,
                 type: 'meat',
                 item: 'oven turkey',
-                imgUrl: 'oven_turey.jpeg',
+                imgUrl: 'oven-turkey.jpg',
                 isChecked: false,
                 madeBy: 'Dad'
             },
@@ -37,7 +37,7 @@ class MenuForm {
                 id: 3,
                 type: 'meat',
                 item: 'ham',
-                imgUrl: 'ham.jpeg',
+                imgUrl: 'Thaksgiving-ham.WEBP',
                 isChecked: false,
                 madeBy: 'Clint'
             },
@@ -45,7 +45,7 @@ class MenuForm {
                 id: 4,
                 type: 'meat',
                 item: 'turducken',
-                imgUrl: 'turducken.jpeg',
+                imgUrl: 'Turducken.jpeg',
                 isChecked: false,
                 madeBy: 'Christian'
             },
@@ -60,8 +60,8 @@ class MenuForm {
             {
                 id: 6,
                 type: 'sides',
-                item: 'turnip greens',
-                imgUrl: 'turnip_greens.jpeg',
+                item: 'green peas',
+                imgUrl: 'greenpeas.jpeg',
                 isChecked: false,
                 madeBy: 'cousin pat'
             },
@@ -69,23 +69,23 @@ class MenuForm {
                 id: 7,
                 type: 'sides',
                 item: 'mashed potatoes w/gravy',
-                imgUrl: 'mashed_potatoes.jpeg',
+                imgUrl: 'mashedpotatoesandgravy.jpeg',
                 isChecked: false,
-                madeBy: 'cousin Bam Bam'
+                madeBy: 'Aunt Tracey'
             },
             {
                 id: 8,
                 type: 'sides',
                 item: 'deep fried loaded baked potato balls',
-                imgUrl: 'baked_potato_balls.jpeg',
+                imgUrl: 'deepfriedloadedbakedpotato balls.jpeg',
                 isChecked: false,
-                madeBy: 'Aunt Tracey'
+                madeBy: 'cousin Bam Bam'
             },
             {
                 id: 9,
                 type: 'sides',
                 item: "mac n' cheese",
-                imgUrl: 'macncheese.jpeg',
+                imgUrl: 'macandcheese.jpeg',
                 isChecked: false,
                 madeBy: 'Aunt Ann' 
             },
@@ -93,15 +93,15 @@ class MenuForm {
                 id: 10,
                 type: 'sides',
                 item: 'cranberry sauce',
-                imgUrl: 'cranberry_sauce.jpeg',
+                imgUrl: 'cranberrysauce.jpeg',
                 isChecked: false,
                 madeBy: 'Aunt Liz'
             },
             {
                 id: 11, 
                 type: 'sides',
-                item: 'greenbean casserole',
-                imgUrl: 'greenbean_casserole.jpeg',
+                item: 'Bacon wraps',
+                imgUrl: 'baconwraps.jpeg',
                 isChecked: false,
                 madeBy: 'Uncle Jason'
             },
@@ -117,15 +117,15 @@ class MenuForm {
                 id: 13, 
                 type: 'desserts',
                 item: 'sweet potato pie',
-                imgUrl: 'sweet_potato_pie.jpeg',
+                imgUrl: 'Sweetpotatopie.jpeg',
                 isChecked: false,
-                madeBy: 'Uncle jimmie'
+                madeBy: 'Uncle Jimmie'
             },
             {
                 id: 14,
                 type: 'desserts',
                 item: 'pecan pie',
-                imgUrl: 'pecan_pie.jpeg',
+                imgUrl: 'pecanpie.jpeg',
                 isChecked: false,
                 madeBy: 'Joey'
             },
@@ -133,37 +133,37 @@ class MenuForm {
                 id: 15, 
                 type: 'desserts',
                 item: 'pound cake',
-                imgUrl: 'pound_cake.jpeg',
+                imgUrl: 'poundcake.jpeg',
                 isChecked: false,
                 madeBy: 'Mikey'
             },
             {
                 id: 16, 
                 type: 'desserts',
-                item: 'bread pudding',
-                imgUrl: 'bread_pudding.jpeg',
+                item: 'pumpkin pie',
+                imgUrl: 'pumpkinpie.jpeg',
                 isChecked: false,
-                madeBy: 'Aunt Punkin'
+                madeBy: "Aunt Punkin"
             },
             {
                 id: 17,
                 type: 'drinks',
                 item: 'ginger ale',
-                imgUrl: 'ginger_ale.jpeg',
+                imgUrl: 'gingerale.jpeg',
                 isChecked: false
             },
             {
                 id: 18,
                 type: 'drinks',
                 item: 'sweet tea',
-                imgUrl: 'sweet_tea.jpeg',
+                imgUrl: 'sweettea.jpeg',
                 isChecked: false
             },
             {
                 id: 19,
                 type: 'drinks',
                 item: 'whiskey',
-                imgUrl: 'whiskey.jpeg',
+                imgUrl: 'coke.jpeg',
                 isChecked: false
             },
             {
@@ -198,31 +198,31 @@ class MenuForm {
             // for each object build Figure
             
             const column = document.createElement('div')
-            column.classList.add('col')
+            column.classList.add('col', 'figs')
 
             column.innerHTML = `
-                <div class="figure-div" data-isChecked=${obj.isChecked}>
+                <div class="figure-div h-100" data-isChecked=${obj.isChecked}>
                     <figure class="figure item-figure">
-                        <img src="https://via.placeholder.com/200x200" alt="placeholder img" class="img-fluid image figure-img food-image rounded" />
+                        <img src="images/${obj.imgUrl}" alt="${obj.item} made by ${obj.madeBy}" class="img-fluid image figure-img food-image w-100 rounded" />
                         <figcaption class="figure-caption food-caption">${
                             obj.hasOwnProperty('madeBy') ? obj.madeBy : ''
                         }</figcaption>
+                        <h3 class="food-heading">${obj.item}</h3>
+                        <div class="form-check figure-form">
+                            <input 
+                                type="checkbox" 
+                                name="${obj.type}" 
+                                id="${obj.type}-${obj.id}"
+                                value="${obj.item}"
+                                class="form-check-input"
+                            />
+                            <label 
+                                for="${obj.type}-${obj.id}" 
+                                class="text-capitalize form-check-label figure-label">
+                                ${obj.item}
+                            </label>
+                        </div>
                     </figure>
-                    <h3 class="food-heading">${obj.item}</h3>
-                    <div class="form-check">
-                        <input 
-                            type="checkbox" 
-                            name="${obj.type}" 
-                            id="${obj.type}-${obj.id}"
-                            value="${obj.item}"
-                            class="form-check-input"
-                        />
-                        <label 
-                            for="${obj.type}-${obj.id}" 
-                            class="text-capitalize form-check-label">
-                            ${obj.item}
-                        </label>
-                    </div>
                 </div>
             `
             switch (obj.type) {
@@ -247,7 +247,7 @@ class MenuForm {
     buildPlate() {
         const person =  document.getElementById('person').value
         const checkboxes = document.querySelectorAll('input[type=checkbox]')
-        const foodItems = document.querySelectorAll('.figure-div')
+        // const foodItems = document.querySelectorAll('.figure-div')
 
 
         checkboxes.forEach(checkbox => {
@@ -258,12 +258,21 @@ class MenuForm {
                 // console.log(checkbox.value)
                 // console.log(name, value)
                 this.plate = {
+                    // spread the old object
                     ...this.plate,
+                    // replace the old values with the ones
                     person,
+                    /**
+                     * 
+                     * This weird thing will take each 
+                     * key and then will spread the array and add each new value
+                     *
+                     */
                     [name]: [...this.plate[name],value]
                     
                 }
 
+                
                 this.menu.forEach(item => {
                     if (checkbox.value == item.item) {
                         item.isChecked = checkbox.checked
@@ -275,7 +284,7 @@ class MenuForm {
         // console.log(this.plate)
         const personPlate = document.getElementById('personPlate')
         personPlate.innerText = `${this.plate.person}'s `
-
+        
         this.makeReceipt(this.menu)
     }
 
@@ -284,7 +293,7 @@ class MenuForm {
         for (let i = 0; i < arr.length; i++) {
             if (arr[i].isChecked) {
                 const listItem = document.createElement('li')
-                listItem.classList.add('list-group-item')
+                listItem.classList.add('list-group-item', 'li')
                 listItem.innerText = arr[i].item
     
                 this.foodList.appendChild(listItem)
@@ -307,6 +316,7 @@ submitBtn.addEventListener('click', (e)=> {
     e.preventDefault()
     // console.log('click')
     action.buildPlate()
+    submitBtn.setAttribute('disabled', true)
 })
 
 
